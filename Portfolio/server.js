00843548f416293c1408a8fe8ce39664b1745339
MassimilianoMarcello <massimarcello@hotmail.it>
2021-09-01 09:51:59 +0200

    upload

--- Portfolio/server.js
+++ Portfolio/server.js
@@ -1,35 +1,31 @@
 const express = require('express');
-
-const path = require('path');
-const routes= require('./routes');
-
 const app = express();
-const port = 3000;   // select a port
-// il seguente pezzo di codice middelware(express.static)ascolta 
-// all'interno della cartella selezionata per vedere se ci sono ulteriori elementi da caricare
+const port = 3001;   
+const path = require('path');
+
+// const routes= require('./routes');
+
 app.use(express.static(path.join(__dirname, './static')));
 
-app.use('/', routes());
+// app.use('/', routes);
 
-// ejs code start
 app.set('view engine','ejs');
 app.set('views', path.join(__dirname, './views'));
-// ejs code end
 
 
+app.get('/', (req, res) => {
+  res.render('index')
+})
+
+app.get('/projects', (req, res) => {
+  res.render('myprojects')
+})
+
+app.get('/contact', (req, res) => {
+  res.render('contact')
+})
 
 
-app.get('/myprojects', (request, response) => {
-  response.sendFile(path.join(__dirname, './static/myprojects.html'));
-});
-
-app.get('/myskills', (request, response) => {
-  response.sendFile(path.join(__dirname, './static/myskills.html'));
-});
-
-app.get('/contact', (request, response) => {
-  response.sendFile(path.join(__dirname, './static/contact.html'));
-});
 // listen port
 app.listen(port, () => {
   // eslint-disable-next-line no-console
